65d21479bfcd65f6995abcf47eb180aefdb63592
Dunsinola <dunsinolaleye@yahoo.com>
2023-02-02 15:33:26 +0100

    2nd commit

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
+    res.redirect('https://roaring-zuccutto-ded008.netlify.app/')
   );
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
