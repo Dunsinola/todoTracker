c2d22d44bd41738d561ee6a6da6b0ad96d912914
Dunsinola <dunsinolaleye@yahoo.com>
2023-02-02 14:50:46 +0100

    2nd commit

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,19 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
+
+
 connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://roaring-zuccutto-ded008.netlify.app/'
+}));
 
 app.use('/api/todos', require('./routes/todoRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
